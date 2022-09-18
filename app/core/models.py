a3173a7d18af53b6be2c68eb7f30bff8bc84efe3
DjangoRepoMngr <98884546+DjangoRepoMngr@users.noreply.github.com>
2022-09-18 13:06:49 +0430

    created model Recipe

--- app/core/models.py
+++ app/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -36,3 +37,20 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = 'email'
+
+
+class Recipe(models.Model):
+    """Recipe object"""
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    descrip0tion = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    link = models.CharField(max_length=255, blank=True)
+
+
+    def __str__(self):
+        return self.title
