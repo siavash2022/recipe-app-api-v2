678d284be36d81cac306a3094e4aab0c0a1d0368
siavash2022 <97461432+siavash2022@users.noreply.github.com>
2022-10-21 17:12:13 +0330

    finish project

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -12,13 +12,36 @@ from core.models import Recipe, Tag,Ingredient
 from recipe import serializers
 import user
 
+from drf_spectacular.utils import (
+    extend_schema_view,
+    extend_schema,
+    OpenApiParameter,
+    OpenApiTypes,
+)
+
+
 from rest_framework import (
     viewsets,
     mixins,
     status
 )
 
-
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'tags',
+                OpenApiTypes.STR,
+                description='Comma separated list of tag IDs to filter',
+            ),
+            OpenApiParameter(
+                'ingredients',
+                OpenApiTypes.STR,
+                description='Comma separated list of ingredient IDs to filter',
+            ),
+        ]
+    )
+)
 class RecipeViewSets(viewsets.ModelViewSet):
     """ view for mange recipe apis """
 
@@ -28,8 +51,24 @@ class RecipeViewSets(viewsets.ModelViewSet):
     permission_classes = [IsAuthenticated]
 
 
+    def _params_to_ints(self,qs):
+        """ convert a list of strings to ints. """
+        return [ int(str_id) for str_id in qs.split(',') ]
+
     def get_queryset(self):
-        return self.queryset.filter(user = self.request.user).order_by('-id')
+        tags = self.request.query_params.get('tags')
+        ingredients = self.request.query_params.get('ingredients')
+        queryset =  self.queryset
+
+        if tags:
+            tag_ids_list = self._params_to_ints(qs=tags)
+            queryset = queryset.filter(tags__id__in = tag_ids_list)
+
+        if ingredients:
+            ingredient_ids_list = self._params_to_ints(qs=ingredients)
+            queryset = queryset.filter(ingredients__id__in = ingredient_ids_list)
+
+        return queryset.filter(user = self.request.user).order_by('-id').distinct()
 
 
     def get_serializer_class(self):
@@ -56,27 +95,46 @@ class RecipeViewSets(viewsets.ModelViewSet):
 
 
 
-class TagViewSet(viewsets.ModelViewSet):
-    """ view for mange Tag apis """
+@extend_schema_view(
+    list=extend_schema(
+        parameters=[
+            OpenApiParameter(
+                'assigned_only',
+                OpenApiTypes.INT, enum=[0, 1],
+                description='Filter by items assigned to recipes.',
+            ),
+        ]
+    )
+)
+class BaseRecipeAttrViewSet(mixins.DestroyModelMixin,
+                            mixins.UpdateModelMixin,
+                            mixins.ListModelMixin,
+                            viewsets.GenericViewSet):
+    """Base viewset for recipe attributes."""
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        """Filter queryset to authenticated user."""
+        assigned_only = bool(
+            int(self.request.query_params.get('assigned_only', 0))
+        )
+        queryset = self.queryset
+        if assigned_only:
+            queryset = queryset.filter(recipe__isnull=False)
+
+        return queryset.filter(
+            user=self.request.user
+        ).order_by('-name').distinct()
+
+
+class TagViewSet(BaseRecipeAttrViewSet):
+    """Manage tags in the database."""
     serializer_class = serializers.TagSerializer
     queryset = Tag.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-    http_method_names = ['get','head','put','patch','delete']
 
 
-
-    def get_queryset(self):
-        return self.queryset.filter(user = self.request.user).order_by('-id')
-
-
-class IngredientViewSet(mixins.ListModelMixin,mixins.UpdateModelMixin, mixins.DestroyModelMixin , viewsets.GenericViewSet):
-
+class IngredientViewSet(BaseRecipeAttrViewSet):
+    """Manage ingredients in the database."""
     serializer_class = serializers.IngredientSerializer
     queryset = Ingredient.objects.all()
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
-
-    def get_queryset(self):
-        return self.queryset.filter(user = self.request.user).order_by('-id')
